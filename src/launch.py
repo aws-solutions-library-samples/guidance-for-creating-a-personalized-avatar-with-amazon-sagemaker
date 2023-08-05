c1c5d10a2d9998c7b76c4fcd255a0c53dcfb5c37
James Wu <jingswu@amazon.com>
2023-08-05 13:43:56 +0000

    Fix the input boolean arguments

--- src/launch.py
+++ src/launch.py
@@ -3,7 +3,7 @@ import argparse
 from io import BytesIO
 from pathlib import Path
 import os
-from utils import preprocess_training_images
+from utils import preprocess_images
 import shutil
 import tarfile
 import uuid
@@ -54,29 +54,12 @@ def parse_arge():
                         default=1, 
                         help="Number of mini-batch to accumlate gradient")
     
-    parser.add_argument("--bf16",
-                        type=bool,
-                        default=True,
-                        help="Whether to use bf16")
-    
-    parser.add_argument("--eight_bit_adam",
-                        type=bool,
-                        default=True,
-                        help="Whether to use 8bit Adam optimization")
-    
-    parser.add_argument("--gradient_checkpointing",
-                        type=bool,
-                        default=True,
-                        help="Whether to save checkpoints")
-    
     parser.add_argument("--train_text_encoder",
-                        type=bool,
-                        default=True,
+                        action='store_true', 
                         help="Whether to fine-tune text encoder")
     
     parser.add_argument("--prior_preservation",
-                        type=bool,
-                        default=True,
+                        action='store_true', 
                         help="Whether to generate class images")
     
     parser.add_argument("--prior_loss_weight", 
@@ -129,9 +112,8 @@ def parse_arge():
                         default=0.05, 
                         help="LoRA text encoder dropout parameter")
     
-    parser.add_argument("--face_cropping", 
-                        type=bool, 
-                        default=True, 
+    parser.add_argument("--face_preprocessing", 
+                        action='store_true', 
                         help="Whether to crop faces")
 
     args, _ = parser.parse_known_args()
@@ -143,36 +125,7 @@ def main():
     
     args = parse_arge()
     
-    print("print all arguments for debugging ===================================")
-    
-    print(f"input_data: {args.input_data} and data type: {type(args.input_data)}")
-    print(f"face_cropping: {args.face_cropping} and data type: {type(args.face_cropping)}")
-    print(f"resolution: {args.resolution} and data type: {type(args.resolution)}")
-    print(f"num_steps: {args.num_steps} and data type: {type(args.num_steps)}")
-    print(f"concept_prompt: {args.concept_prompt} and data type: {type(args.concept_prompt)}")
-    print(f"lr: {args.lr} and data type: {type(args.lr)}")
-    print(f"grad_accum: {args.grad_accum} and data type: {type(args.grad_accum)}")
-    print(f"bf16: {args.bf16} and data type: {type(args.bf16)}")
-    print(f"eight_bit_adam: {args.eight_bit_adam} and data type: {type(args.eight_bit_adam)}")
-    print(f"gradient_checkpointing: {args.gradient_checkpointing} and data type: {type(args.gradient_checkpointing)}")
-    print(f"train_text_encoder: {args.train_text_encoder} and data type: {type(args.train_text_encoder)}")
-    print(f"prior_preservation: {args.prior_preservation} and data type: {type(args.prior_preservation)}")
-    print(f"prior_loss_weight: {args.prior_loss_weight} and data type: {type(args.prior_loss_weight)}")
-    print(f"class_prompt: {args.class_prompt} and data type: {type(args.class_prompt)}")
-    print(f"num_class_images: {args.num_class_images} and data type: {type(args.num_class_images)}")
-    print(f"lora_r: {args.lora_r} and data type: {type(args.lora_r)}")
-    print(f"lora_alpha: {args.lora_alpha} and data type: {type(args.lora_alpha)}")
-    print(f"lora_bias: {args.lora_bias} and data type: {type(args.lora_bias)}")
-    print(f"lora_dropout: {args.lora_dropout} and data type: {type(args.lora_dropout)}")
-    print(f"lora_text_encoder_r: {args.lora_text_encoder_r} and data type: {type(args.lora_text_encoder_r)}")
-    print(f"lora_text_encoder_alpha: {args.lora_text_encoder_alpha} and data type: {type(args.lora_text_encoder_alpha)}")
-    print(f"lora_text_encoder_bias: {args.lora_text_encoder_bias} and data type: {type(args.lora_text_encoder_bias)}")
-    print(f"lora_text_encoder_dropout: {args.lora_text_encoder_dropout} and data type: {type(args.lora_text_encoder_dropout)}")
-    
-    
-    print("end of all arguments for debugging ===================================")
-    
-    train_path = preprocess_training_images(args.input_data) #, args.face_cropping)
+    train_path = preprocess_images(args.input_data, args.face_preprocessing)
  
     print("list image files for trianing input: \n\n")
     print(os.listdir(str(train_path)))
@@ -200,9 +153,9 @@ def main():
         concept_prompt=args.concept_prompt,
         learning_rate=args.lr,
         gradient_accumulation=args.grad_accum,
-        fp16=args.bf16,
-        use_8bit_adam=args.eight_bit_adam,
-        gradient_checkpointing=args.gradient_checkpointing,
+        fp16=True,
+        use_8bit_adam=True,
+        gradient_checkpointing=True,
         train_text_encoder=args.train_text_encoder,
         with_prior_preservation=args.prior_preservation,
         prior_loss_weight=args.prior_loss_weight,
--- src/utils.py
+++ src/utils.py
@@ -10,35 +10,8 @@ def extract_tar(tar_obj, extract_path):
     
     with tarfile.open(fileobj=tar_obj) as tar:
         tar.extractall(extract_path)
-
-def resize(image_path):
-    
-    target_size = (512, 512)
-    
-    image = Image.open(image_path)
-    # Calculate the aspect ratio
-    width, height = image.size
-    aspect_ratio = width / height
-    
-    # Calculate the new dimensions for scaling down
-    if width > height:
-        new_height = target_size[1]
-        new_width = int(new_height * aspect_ratio)
-    else:
-        new_width = target_size[0]
-        new_height = int(new_width / aspect_ratio)
-    
-    # Resize the image while maintaining aspect ratio
-    image = image.resize((new_width, new_height), Image.Resampling.LANCZOS)
-    image_size = image.size
-    
-    x = (image_size[0] - target_size[0]) // 2
-    y = (image_size[1] - target_size[1]) // 2
-    image = image.crop((x, y, x + target_size[0], y + target_size[1]))
-    
-    return image
-    
-def detect_and_resize(image_path):
+        
+def detect_face_and_resize(image_path):
     """Crop around face and resize an image to 512x512."""
     
     cropper = Cropper(width=512, height=512)
@@ -49,41 +22,46 @@ def detect_and_resize(image_path):
     
     return Image.fromarray(cropped_array)
 
-def preprocess_images(tar_obj):
-    
-    tmp_path = Path(f"/tmp/{random.randint(0, 1000000)}")
-    while tmp_path.exists():
-        tmp_path = Path(f"/tmp/{random.randint(0, 1000000)}")
-    tmp_path.mkdir(parents=True, exist_ok=False)
-    
-    extract_tar(tar_obj, tmp_path)
-    
-    dest_path = tmp_path / "cropped"
-    dest_path.mkdir(parents=True, exist_ok=True)
-    
-    for n,img_path in enumerate(chain(tmp_path.glob("*.[jJ][pP][Gg]"),tmp_path.glob("*.[Pp][Nn][Gg]"))):
-        try:
-            cropped = detect_and_resize(img_path.as_posix())
-            cropped.save(dest_path / f"image_{n}.png")
-        except ValueError:
-            print(f"Could not detect face in {img_path}. Skipping.")
-            continue
-    
-    return dest_path
+def resize_and_center_crop(img_path):
+    img = Image.open(img_path)
+    width, height = img.size
 
-def preprocess_training_images(image_path):#, face_cropping):
+    # Calculate dimensions for the crop
+    if width > height:
+        left = (width - height) / 2
+        top = 0
+        right = (width + height) / 2
+        bottom = height
+    else:
+        top = (height - width) / 2
+        left = 0
+        bottom = (height + width) / 2
+        right = width
+
+    # Crop to a square
+    img = img.crop((left, top, right, bottom))
+
+    # Resize to 512x512
+    img = img.resize((512, 512))
+
+    return img
+
+
+def preprocess_images(image_path, face_preprocessing):
     
     input_path = Path(image_path)
     
     dest_path = input_path / "cropped"
     dest_path.mkdir(parents=True, exist_ok=True)
     
+    print(face_preprocessing)
+    
+    crop_func = detect_face_and_resize if face_preprocessing else resize_and_center_crop
+    
     for n,img_path in enumerate(chain(input_path.glob("*.[jJ][pP][Gg]"),input_path.glob("*.[Pp][Nn][Gg]"))):
         try:
-            # if face_cropping:
-            cropped = detect_and_resize(img_path.as_posix())
-            # else:
-            #     cropped = resize(img_path.as_posix())
+            
+            cropped = crop_func(img_path.as_posix())
                 
             cropped.save(dest_path / f"image_{n}.png")
         except ValueError:
